ec505446b123552f2d5a45803cb8d217b6e7862c
Can Soylu <hsyncn17@gmail.com>
2021-03-12 17:49:08 +0300

    added albums via url

--- artist.js
+++ artist.js
@@ -24,3 +24,41 @@ function removeShow() {
 
 //listen for tab click
 tabItems.forEach((item) => item.addEventListener("click", selectItem));
+
+const makeAlbum = document.querySelector("#benim-album");
+
+const createAlbum = (deneme) => {
+  const div = document.createElement("div");
+
+  div.classList.add("col-lg-2");
+  div.innerHTML = `
+                          <a href="/album.html">
+                            <div class="card border-0">
+                             <img src="${deneme.cover}" alt="..."/>
+                             <div class="card-body">
+                               <p class="card-text">${deneme.title}</p>
+                               <p class="text-center">Queen</p>
+                             </div>
+                          </div>
+                        </a>
+                  `;
+  makeAlbum.appendChild(div);
+};
+
+let album = [];
+
+const getAlbum = () => {
+  fetch(`https://striveschool-api.herokuapp.com/api/deezer/artist/412/albums`)
+    .then((response) => response.json())
+    .then((bal) => {
+      console.log(bal.data);
+      album = bal.data;
+      bal.data.forEach((item) => createAlbum(item));
+    });
+};
+
+console.log(album);
+
+window.onload = function () {
+  getAlbum();
+};
